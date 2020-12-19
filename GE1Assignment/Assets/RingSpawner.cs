e94362fc4ea5eee388a3ab881337da8cb235505a
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-12-19 15:12:29 +0000

    Ring dynamically generated

--- GE1Assignment/Assets/RingSpawner.cs
+++ GE1Assignment/Assets/RingSpawner.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class RingSpawner : MonoBehaviour
 {
-    public int segments = 10;
+    public int radius = 10;
 
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
         Vector3 point = new Vector3(0, 0, 0);
-        float thetaInc = (Mathf.PI * 2.0f) / (float)segments;
-        float radius = 10.0f;
 
+        float cubeY = prefab.transform.localScale.y;
+        float circumfrence = (2.0f * Mathf.PI * radius * 0.95f);
+        int segments = (int) (Mathf.Floor(circumfrence / cubeY));
+
+        float thetaInc = (Mathf.PI * 2.0f) / (float)segments;
         float theta = Mathf.PI * 2.0f / ((float) segments);
         float z = 0.0f;
         for (int j = 0 ; j < segments ; j ++)
