fa926db93dea6822d08274d0e4238f1aa79868ed
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-12-19 18:04:57 +0000

    There was no need for meshes or triangles. Rings are now generating

---
+++ GE1Assignment/Assets/AddTriangle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTriangle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject tri = new GameObject();
+        tri.transform.parent = this.transform;
+
+        Vector3 pos1 = this.transform.TransformPoint(0.5f, 0.5f, -0.5f);
+        Vector3 pos2 = this.transform.TransformPoint(0.5f, 0.5f, -0.5f);
+        Vector3 pos3 = this.transform.TransformPoint(-0.5f, 1, -0.5f);
+
+        tri.AddComponent<MeshFilter>();
+        tri.AddComponent<MeshRenderer>();
+        Mesh mesh = tri.GetComponent<MeshFilter>().mesh;
+
+        mesh.Clear();
+
+        print(pos1);
+
+        //mesh.vertices = new Vector3[] {new Vector3(0.5f, 0.5f, -0.5f), new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(-0.5f, 1, -0.5f)};
+        mesh.vertices = new Vector3[] {pos1, pos2, pos3};
+        //mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        mesh.triangles =  new int[] {0, 1, 2};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- GE1Assignment/Assets/RingSpawner.cs
+++ GE1Assignment/Assets/RingSpawner.cs
@@ -6,18 +6,30 @@ using UnityEngine;
 public class RingSpawner : MonoBehaviour
 {
     public int radius = 10;
+    public int startingRings = 1;
 
     public GameObject prefab;
+
+    void Awake() {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 point = new Vector3(0, 0, 0);
+        for (int i = 0; i < startingRings; i++) {
+            print("Pos: " + transform.position);
+            CreateRing();
+            transform.position += new Vector3(0, 0, 5);
+        }
+    }
+
+    void CreateRing() {
+        Vector3 point = this.transform.position;
 
         float cubeY = prefab.transform.localScale.y;
         float circumfrence = (2.0f * Mathf.PI * radius * 0.95f);
         int segments = (int) (Mathf.Floor(circumfrence / cubeY));
-
-        CreateTriangle(new Vector3(0, 0, 0), new Vector3(0, 5, 0), new Vector3(5, 5, 0));
 
         float thetaInc = (Mathf.PI * 2.0f) / (float)segments;
         float theta = Mathf.PI * 2.0f / ((float) segments);
@@ -32,7 +44,10 @@ public class RingSpawner : MonoBehaviour
             GameObject cube = GameObject.Instantiate<GameObject>(prefab);
                 cube.transform.position = new Vector3(x, y, z);
 
-            Vector3 target = point;
+            //CreateTriangle(new Vector3(0, 0, 0), new Vector3(0, 5, 0), new Vector3(5, 5, 0), cube);
+
+
+            Vector3 target = new Vector3(0, 0, 0);
             Vector3 relativePos = target - cube.transform.position;
             Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.right);
             cube.transform.rotation = rotation;
@@ -42,19 +57,31 @@ public class RingSpawner : MonoBehaviour
                 
             cube.transform.parent = this.transform;
         }
+
     }
 
-    void CreateTriangle(Vector3 v1, Vector3 v2, Vector3 v3) {
-        GameObject tri = new GameObject();
-        tri.AddComponent<MeshFilter>();
-        tri.AddComponent<MeshRenderer>();
-        Mesh mesh = tri.GetComponent<MeshFilter>().mesh;
+    void CreateTriangle(Vector3 v1, Vector3 v2, Vector3 v3, GameObject cube) {
+        // GameObject tri = new GameObject();
+        // tri.transform.parent = cube.transform;
 
-        mesh.Clear();
+        // Quaternion rot = cube.transform.rotation;
 
-        mesh.vertices = new Vector3[] {v1, v2, v3};
-        //mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
-        mesh.triangles =  new int[] {0, 1, 2};
+        // Vector3 pos1 = cube.transform.TransformPoint(0.5f, 0.5f, -0.5f);
+        // Vector3 pos2 = cube.transform.TransformPoint(-0.5f, 0.5f, -0.5f);
+        // Vector3 pos3 = cube.transform.TransformPoint(-0.5f, 1, -0.5f);
+
+        // tri.AddComponent<MeshFilter>();
+        // tri.AddComponent<MeshRenderer>();
+        // Mesh mesh = tri.GetComponent<MeshFilter>().mesh;
+
+        // mesh.Clear();
+
+        // print(pos1);
+
+        // //mesh.vertices = new Vector3[] {new Vector3(0.5f, 0.5f, -0.5f), new Vector3(-0.5f, 0.5f, -0.5f), new Vector3(-0.5f, 1, -0.5f)};
+        // mesh.vertices = new Vector3[] {pos1, pos2, pos3};
+        // //mesh.uv = new Vector2[] {new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1)};
+        // mesh.triangles =  new int[] {0, 1, 2};
     }
 
     GameObject CreateBrick(float x, float y, float z, float xScale = 1.0f, float yScale = 1.0f, float zScale = 1.0f)
