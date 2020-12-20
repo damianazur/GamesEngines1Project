8512904fdc9106f5414c72d347ff2251c1fda7fd
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-12-20 22:41:37 +0000

    Ring move distance calculated using coordinate space in preperation for visualizer

--- GE1Assignment/Assets/RingSpawner.cs
+++ GE1Assignment/Assets/RingSpawner.cs
@@ -11,6 +11,8 @@ public class RingSpawner : MonoBehaviour
     public List<List<GameObject>> movableRingsList = new List<List<GameObject>>();
     public List<List<GameObject>> endRings = new List<List<GameObject>>();
 
+    private Vector3 ringGeneratePoint;
+
     private float movedDistance = 0;
     private int positionOffset;
     public GameObject prefab;
@@ -24,7 +26,14 @@ public class RingSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {   
+        generateInitalRings();
+    }
+
+    void generateInitalRings() {
+        // Last x many rings will have their radius gradually reduced
         int finalRingCount = 50;
+        
+        float savedRadius = radius;
         // Create the initial rings
         for (int i = 0; i < startingRings; i++) {
             List<GameObject> ringSegments = CreateRing();
@@ -46,12 +55,14 @@ public class RingSpawner : MonoBehaviour
 
         int endPieceCount = endRings.Count;
         transform.position -= new Vector3(0, 0, (endPieceCount + 1) * positionOffset);
-        radius = 10;
+        // Radius is reset 
+        radius = savedRadius;
 
         // Add final ring so there is no gap
         CreateRing();
     }
 
+    // Creates a ring in the position of the spawner
     List<GameObject> CreateRing() {
         // The ring is made up of rectangles (segments)
         List<GameObject> ringSegments = new List<GameObject>();
@@ -60,10 +71,12 @@ public class RingSpawner : MonoBehaviour
         Vector3 centerPoint = this.transform.position;
 
         // Calculate the number of segments needed to fill the radius of the circle
-        float cubeGap = 1;
+        float segmentGap = 1; Random.Range(0, 5);
+        // float segmentGap = 1;
         float cubeY = prefab.transform.localScale.y;
-        float circumfrence = (2.0f * Mathf.PI * (radius - cubeGap));
+        float circumfrence = (2.0f * Mathf.PI * (radius - segmentGap));
         float yScale = (float) (circumfrence / noOfSegments);
+        // Size scale used for resizing segments to fit the radius
         float sizeScale = yScale / cubeY;
 
         // Get the angle of the segment on the circle (2 pi = 360 degress => 360 / noOfSegments = angle for of each segment)
@@ -80,9 +93,11 @@ public class RingSpawner : MonoBehaviour
             GameObject cube = GameObject.Instantiate<GameObject>(prefab);
                 cube.transform.position = new Vector3(x, y, z);
 
-            float xScale = prefab.transform.localScale.z;
+            // Resize the cube
+            float xScale = prefab.transform.localScale.x;
+            // print(xScale);
             if (endRings.Count > 0 && endRings.Count < 50) {
-                xScale = xScale + (endRings.Count * 4);
+                xScale = 1 + (endRings.Count * 4);
             }
             cube.transform.localScale = new Vector3(
                 xScale,
@@ -105,59 +120,72 @@ public class RingSpawner : MonoBehaviour
         return ringSegments;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float speed = 10.0f;
+    void moveRingsForward(float speed) {
         // Move the segments along the z-axis each frame
         foreach (List<GameObject> ringSegments in movableRingsList) {
             foreach (GameObject segment in ringSegments) {
                 segment.transform.position -= new Vector3(0, 0, Time.deltaTime * speed);
             }
         }
+    }
+
+    void generateAndSyncNewRing() {
+        // Create and synchronize rings
+        // numOfRings is used to generate multiple rings in the scenario of a lag spike
+        int numOfRings = (int) Mathf.Floor(movedDistance / positionOffset);
+
+        List<GameObject> lastRing = movableRingsList[movableRingsList.Count - 1];
+        float zCoord = lastRing[0].transform.position.z;
+
+        // There has to be at least one ring but can be more than one at a time
+        if (numOfRings == 0) {
+            numOfRings = 1;
+        }
+        // Change the position of segments in the ring
+        for (int i = 0; i < numOfRings; i++) {
+            List<GameObject> ringSegments = CreateRing();
+
+            // Spawn the ring and set it's position relative to the previous one
+            foreach (GameObject segment in ringSegments) {
+                float segX = segment.transform.position.x;
+                float segY = segment.transform.position.y;
+
+                // Offset the ring being the last (hence why it's "synchronising" there will be no gaps)
+                segment.transform.position = new Vector3(segX, segY, zCoord + (positionOffset + i * positionOffset));
+            }
+
+            movableRingsList.Add(ringSegments);
+            
+            // Destory the objects that are no longer used
+            int destroyIndex = movableRingsList[0].Count - 1;
+            while (destroyIndex > -1) {
+                Destroy(movableRingsList[0][destroyIndex]);
+                destroyIndex -= 1;
+            }
+
+            movableRingsList.RemoveAt(0);
+            // movedDistance -= (float) positionOffset;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // radius = Random.Range(3, 10);
+        float speed = 30.0f;
+        moveRingsForward(speed);
+
+        Vector3 latestRingCenter = new Vector3(transform.position.x, transform.position.y, movableRingsList[movableRingsList.Count -1][0].transform.position.z);
+        float movedDistance = Vector3.Distance (latestRingCenter, transform.position);
+        print(movedDistance);
 
         // Calculate how far the rings have moved so that more rings can be spawned
-        movedDistance += Time.deltaTime * speed;
+        // movedDistance += Time.deltaTime * speed;
 
         // If the rings have moved a certain distance then spawn in a new ring
         if (movedDistance > 5) {
-            List<GameObject> lastRing = movableRingsList[movableRingsList.Count - 1];
-            float zCoord = lastRing[0].transform.position.z;
-
-            // Create and synchronize rings
-            // numOfRings is used to generate multiple rings in the scenario of a lag spike
-            int numOfRings = (int) Mathf.Floor(movedDistance / positionOffset);
-            // There has to be at least one ring but can be more than one at a time
-            if (numOfRings == 0) {
-                numOfRings = 1;
-            }
-            // Change the position of segments in the ring
-            for (int i = 0; i < numOfRings; i++) {
-                List<GameObject> ringSegments = CreateRing();
-
-                // Spawn the ring and set it's position relative to the previous one
-                foreach (GameObject segment in ringSegments) {
-                    float segX = segment.transform.position.x;
-                    float segY = segment.transform.position.y;
-
-                    // Offset the ring being the last (hence why it's "synchronising" there will be no gaps)
-                    segment.transform.position = new Vector3(segX, segY, zCoord + (positionOffset + i * positionOffset));
-                }
-
-                movableRingsList.Add(ringSegments);
-                
-                // Destory the objects that are no longer used
-                int destroyIndex = movableRingsList[0].Count - 1;
-                while (destroyIndex > -1) {
-                    Destroy(movableRingsList[0][destroyIndex]);
-                    destroyIndex -= 1;
-                }
-
-                movableRingsList.RemoveAt(0);
-                movedDistance -= (float) positionOffset;
-            }
-
-            // print(GameObject.FindGameObjectsWithTag("RingSegment").Length);
+            generateAndSyncNewRing();
         }
     }
 }
