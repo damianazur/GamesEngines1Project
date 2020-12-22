8270936eeacf906f176f343785ec0f4609d70d36
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-12-22 16:24:12 +0000

    last rings for infinity effect are kept consistent

--- GE1Assignment/Assets/RingSpawner.cs
+++ GE1Assignment/Assets/RingSpawner.cs
@@ -75,7 +75,12 @@ public class RingSpawner : MonoBehaviour
         Vector3 centerPoint = this.transform.position;
 
         // Calculate the number of segments needed to fill the radius of the circle
-        float segmentGap = Random.Range(0, 5);
+        float segmentGap;
+        if (endRings.Count > 0 && endRings.Count < 50) {
+            segmentGap = 1;
+        } else {
+            segmentGap = Random.Range(0, 5);
+        }
         // float segmentGap = 1;
         float cubeY = prefab.transform.localScale.y;
         float circumfrence = (2.0f * Mathf.PI * (radius - segmentGap));
