d034a4aca81a926728aaa57a10647528a83a7b2e
Damian Wojtowicz <47143782+damianazur@users.noreply.github.com>
2020-12-22 17:05:27 +0000

    Create pulsating cube script and inital variables

---
+++ GE1Assignment/Assets/PulsatingCubeVis.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PulsatingCubeVis : MonoBehaviour
+{
+    public int cubeCount = 30;
+    public int cubeMinSize = 1;
+    public int cubeMaxSize = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCubes();
+    }
+
+    private void spawnCubes() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
